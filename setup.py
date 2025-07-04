5498bb0bb8cbeb00e8aacb42aeec84712a118f98
Ai Engineer <bittush9534@gmail.com>
2025-07-04 19:21:26 +0530

    Updated setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Bittu sharma',
+    author_email= 'bittush@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
